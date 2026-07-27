80cdfd6fabd3d64043a80a3eb3a7057b5a828909
Anis-Dz3 <anisxbochemat@gmail.com>
2026-07-27 11:05:39 +0100

    features

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import Features from "@/components/Features";
+import WidgetBuilderPage from "@/components/WidgetBuilder";
 
 export default function DashboardPage() {
   return (
     <main className="min-h-screen bg-purple-50 text-gray-900">
       <Header/>
       <Hero/>
+      <Features/>
+      <WidgetBuilderPage/>
     </main>
   );
 }
