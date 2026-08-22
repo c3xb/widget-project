95b9b3d05efb55716371b69ed2330eb62823d6b9
Anis-Dz3 <anisxbochemat@gmail.com>
2026-08-22 19:13:24 +0100

    gg

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-<<<<<<< HEAD
-     //  allowedDevOrigins: [ '192.168.0.135']
-=======
-     allowedDevOrigins: [ '192.168.0.135'],
-     devIndicators : false
->>>>>>> a2ed180c973a52c55edeab2885d77e031595c5aa
+  allowedDevOrigins: ['192.168.0.135'],
+  devIndicators: false,
 };
 
 export default nextConfig;
